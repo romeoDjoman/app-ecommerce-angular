6e75f08747543b145406890125836240ca143dcf
romeoDjoman <romeo.djoman@outlook.fr>
2025-02-20 12:04:46 +0100

    Ajout reussi des packages de fontawesome et tailwind css

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import {APP_ROUTES} from './app.routes';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 // import {tokenInterceptor} from './core/auth/token.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(APP_ROUTES),
-    provideHttpClient()
+    provideHttpClient(), provideAnimationsAsync()
     // provideHttpClient(withInterceptors([tokenInterceptor]))
   ]
 };
