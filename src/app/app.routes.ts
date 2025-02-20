8ed5fd13d37012c62412c2717e78f864155360fa
romeoDjoman <romeo.djoman@outlook.fr>
2025-02-20 18:38:55 +0100

    Gestion des connexion et role

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
-import {AppComponent} from './app.component';
+import { roleGuard } from './core/auth/role.guard';
+import {AdminDashboardComponent} from './roles/admin/admin-dashboard/admin-dashboard.component';
 
 export const APP_ROUTES: Routes = [
   {
@@ -9,48 +10,40 @@ export const APP_ROUTES: Routes = [
   },
   {
     path: '',
-    redirectTo: 'auth/login',
+    redirectTo: '/auth/login',
     pathMatch: 'full'
   },
   {
     path: '**',
-    redirectTo: 'auth/login'
+    redirectTo: '/auth/login'
+  },
+
+  {
+    path: 'admin',
+    component: AdminDashboardComponent,
+    canActivate: [roleGuard],
+    data: { roles: ['ADMIN'] },
+  },
+  {
+    path: 'author',
+    loadChildren: () =>
+      import('./roles/author/author.routes').then((m) => m.AUTHOR_ROUTES),
+    canActivate: [roleGuard],
+    data: { roles: ['AUTHOR'] },
+  },
+  {
+    path: 'reviewer',
+    loadChildren: () =>
+      import('./roles/reviewer/reviewer.routes').then((m) => m.REVIEWER_ROUTES),
+    canActivate: [roleGuard],
+    data: { roles: ['REVIEWER'] },
+  },
+  {
+    path: 'user',
+    loadChildren: () =>
+      import('./roles/user/user.routes').then((m) => m.USER_ROUTES),
+    canActivate: [roleGuard],
+    data: { roles: ['USER'] },
   }
 ];
 
-
-
-/*
-import { Routes } from '@angular/router';
-import { roleGuard } from './guards/role.guard';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './pages/auth/login/login.component';
-
-export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  {
-    path: 'dashboard',
-    component: DashboardComponent,
-    canActivate: [roleGuard('ADMIN')],
-  },
-  { path: '**', redirectTo: 'login' },
-];
-*/
-
-/*
-import { Routes } from '@angular/router';
-import { roleGuard } from './guards/role.guard';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './pages/auth/login/login.component';
-
-export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  {
-    path: 'dashboard',
-    component: DashboardComponent,
-    canActivate: [roleGuard],
-    data: { expectedRole: 'ADMIN' },
-  },
-  { path: '**', redirectTo: 'login' },
-];
-*/
---
+++ src/app/roles/reviewer/reviewer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reviewer',
+  imports: [],
+  templateUrl: './reviewer.component.html',
+  styleUrl: './reviewer.component.css'
+})
+export class ReviewerComponent {
+
+}
